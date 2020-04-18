b212e9b89beaf8d86ed4ff5c32d5fa861d96e729
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-04-18 18:30:41 +0200

    Unit test of the unary operators.

---
+++ NumericArray/unittests/src/unittest_unary.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include "TestArrayEqual.h"
+
+using  Vector = cs::NumericArray<double,uint8_t,3,1>;
+using value_T = Vector::value_type;
+
+using Values = std::initializer_list<value_T>;
+
+namespace test_unary {
+
+  TEST_CASE("cs::Array<> unary +.", "[unary][plus]") {
+    const Vector x{1, 2, 3};
+
+    const Vector y = +x;
+    REQUIRE( equals0(y, Values{1, 2, 3}) );
+  }
+
+  TEST_CASE("cs::Array<> unary -.", "[unary][minus]") {
+    const Vector x{1, 2, 3};
+
+    const Vector y = -x;
+    REQUIRE( equals0(y, Values{-1, -2, -3}) );
+  }
+
+} // namespace test_unary
