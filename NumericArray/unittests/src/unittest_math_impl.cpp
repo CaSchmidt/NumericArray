d92830bfab4debc2e2713d598e98382d30f1cb92
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-04-26 18:36:50 +0200

    Adjust to new naming conventions.

--- NumericArray/unittests/src/unittest_math_impl.cpp
+++ NumericArray/unittests/src/unittest_math_impl.cpp
@@ -4,22 +4,22 @@ namespace test_binary {
 
   float test_invSqrt(const float x)
   {
-    return cs::invSqrt(x);
+    return csInvSqrt(x);
   }
 
   float test_sqrt(const float x)
   {
-    return cs::sqrt(x);
+    return csSqrt(x);
   }
 
   double test_invSqrt(const double x)
   {
-    return cs::invSqrt(x);
+    return csInvSqrt(x);
   }
 
   double test_sqrt(const double x)
   {
-    return cs::sqrt(x);
+    return csSqrt(x);
   }
 
 } // namespace test_binary
