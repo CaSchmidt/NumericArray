91f9a2c758006c3d8370cacc3e77d071ddde83e1
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-04-28 12:04:26 +0200

    Some more tests to check the generated code.

--- NumericArray/tests/src/test_array.cpp
+++ NumericArray/tests/src/test_array.cpp
@@ -12,6 +12,18 @@ using Vector = cs::NumericArray<value_T,size_T,3,1>;
 Matrix M;
 Vector v;
 
+void test_gemm(const value_T alpha, const Matrix& A, const Matrix& B,
+               const value_T beta, const Matrix& C)
+{
+  M = alpha*A*B + beta*C;
+}
+
+void test_gemv(const value_T alpha, const Matrix& A, const Vector& x,
+               const value_T beta, const Vector& y)
+{
+  v = alpha*(A*x) + beta*y;
+}
+
 void test_axpy(const value_T alpha, const Vector&x, const Vector& y)
 {
   v = alpha*x + y;
@@ -25,6 +37,11 @@ void test_copy(const Vector& x)
 void test_set(const value_T v)
 {
   ::v = v;
+}
+
+value_T test_distance(const Vector& a, const Vector& b)
+{
+  return cs::distance(a, b);
 }
 
 int main(int /*argc*/, char ** /*argv*/)
