e12aa190d5771def3417387fdc7a31ca00d7f50e
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-05-23 18:17:27 +0200

    Refactoring of array tests.

--- NumericArray/tests/src/test_array.cpp
+++ NumericArray/tests/src/test_array.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <iostream>
+
 #include <cs/NumericArray.h>
 
 using  size_T = uint8_t;
@@ -24,79 +26,107 @@ void print(const array_T& array)
   }
 }
 
+#if 0
 void test_gemm(const value_T alpha, const Matrix& A, const Matrix& B,
                const value_T beta, const Matrix& C)
 {
   M = alpha*A*B + beta*C;
 }
+#endif
 
+#if 0
 void test_gemv(const value_T alpha, const Matrix& A, const Vector& x,
                const value_T beta, const Vector& y)
 {
   v = alpha*(A*x) + beta*y;
 }
+#endif
 
+#if 0
 void test_axpy(const value_T alpha, const Vector&x, const Vector& y)
 {
   v = alpha*x + y;
 }
+#endif
 
+#if 1
 void test_copy(const Vector& x)
 {
   v = x;
 }
+#endif
 
+#if 1
 void test_set(const value_T v)
 {
   ::v = v;
 }
+#endif
 
+#if 0
 value_T test_distance(const Vector& a, const Vector& b)
 {
   return cs::distance(a, b);
 }
+#endif
 
+#if 0
 void test_identity()
 {
   M = cs::identity<Matrix::traits_type>();
 }
+#endif
 
+#if 0
 void test_identity(const Vector& x)
 {
   v = cs::identity<Matrix::traits_type>()*x;
 }
+#endif
 
+#if 0
 void test_scale(const value_T sx, const value_T sy, const value_T sz)
 {
   M = cs::scale<Matrix::traits_type>(sx, sy, sz);
 }
+#endif
 
+#if 0
 value_T test_max(const value_T a, const value_T b)
 {
   return csMax(a, b);
 }
+#endif
 
+#if 0
 value_T test_min(const value_T a, const value_T b)
 {
   return csMin(a, b);
 }
+#endif
 
+#if 0
 value_T test_dot1(const Vector& a, const Vector& b)
 {
   return cs::dot1(a, b);
 }
+#endif
 
+#if 0
 void test_clamp(const Vector& x, const value_T lo, const value_T hi)
 {
   v = cs::clamp(x, lo, hi);
 }
+#endif
 
+#if 0
 void test_direction(const Vector& from, const Vector& to)
 {
   v = cs::direction(from, to);
 }
+#endif
 
-#if 1
+#if 0
 namespace test_manip {
 
   using Vector3 = cs::Array<cs::Vector3Manip<cs::RowMajorPolicy<cs::ArrayTraits<double,uint8_t,3,1>>>>;
@@ -112,6 +142,13 @@ namespace test_manip {
   }
 
 } // namespace test_manip
+#endif
+
+#if 1
+void test_simd(const Vector& a, const Vector& b)
+{
+  v = a + b;
+}
 #endif
 
 int main(int /*argc*/, char ** /*argv*/)
