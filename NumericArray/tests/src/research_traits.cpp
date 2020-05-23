7671a531a4481cf2a324fa019df8887909ded69b
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-05-23 18:20:33 +0200

    Update the research on type traits.

--- NumericArray/tests/src/research_traits.cpp
+++ NumericArray/tests/src/research_traits.cpp
@@ -5,37 +5,75 @@
 
 #include <type_traits>
 
-template<typename T, typename = void>
-struct if_member : std::false_type {};
+template<typename T, typename = bool>
+struct if_simd : std::false_type { };
 
 template<typename T>
-struct if_member<T,decltype((void)T::member,void())> : std::true_type {};
+struct if_simd<T,decltype((void)T::is_simd,std::declval<T>().block(0),bool())> : std::true_type { };
 
 template<typename T>
-inline constexpr bool if_member_v = if_member<T>::value;
+inline constexpr bool if_simd_v = if_simd<T>::value;
+
+template<typename T>
+constexpr std::enable_if_t<if_simd_v<T>,bool> check_simd()
+{
+  return static_cast<bool>(T::is_simd);
+}
+
+template<typename T>
+constexpr std::enable_if_t<!if_simd_v<T>,bool> check_simd()
+{
+  return false;
+}
 
 struct A {
-    static constexpr int member = 1;
+#if 1
+  static inline constexpr bool is_simd = true;
+#else
+  static constexpr bool is_simd() 
+  {
+    return true;
+  }
+#endif
+
+  constexpr int block(int) const
+  {
+    return 0;
+  }
 };
 
 struct B {
 };
 
 template<typename T>
-void print(const T& t)
-{
-    if constexpr( if_member_v<T> ) {
-        printf("member\n");
-    } else {
-        printf("no member\n");
-    }
-}
+struct C {
+#if 1
+  static inline constexpr bool is_simd = check_simd<T>();
+#else
+  static constexpr bool is_simd() 
+  {
+    return check_simd<T>();
+  }
+#endif
 
-int main(int,char**)
+  int block(int) const
+  {
+    return 0;
+  }
+
+  T t;
+};
+
+int main(int, char**)
 {
-    A a;
-    B b;
-    print(a);
-    print(b);
-    return EXIT_SUCCESS;
+  if constexpr( check_simd<A>() ) {
+    printf("A\n");
+  }
+  if constexpr( check_simd<B>() ) {
+    printf("B\n");
+  }
+  if constexpr( check_simd< C<C<A>> >() ) {
+    printf("C\n");
+  }
+  return EXIT_SUCCESS;
 }
