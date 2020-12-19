4a9bdcd8e7215211479c9f1b366a895610ae489d
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-12-19 16:52:22 +0100

    Refactoring of the math unit tests.

--- NumericArray/unittests/src/unittest_math.cpp
+++ NumericArray/unittests/src/unittest_math.cpp
@@ -1,27 +1,11 @@
-#define CATCH_CONFIG_MAIN
+#include <iostream>
+
 #include <catch.hpp>
 
 #include "TestEquals.h"
 #include "TestFormatter.h"
 
 namespace test_binary {
-
-  template<typename T>
-  struct Type {
-    // SFINAE
-  };
-
-  template<>
-  struct Type<float> {
-    static const char *info;
-  };
-  const char *Type<float>::info = "float";
-
-  template<>
-  struct Type<double> {
-    static const char *info;
-  };
-  const char *Type<double>::info = "double";
 
   extern float test_invSqrt(const float x);
   extern float test_sqrt(const float x);
@@ -32,13 +16,13 @@ namespace test_binary {
   TEMPLATE_TEST_CASE("IEEE 754 binary floating point.", "[binary]", float, double) {
     using Fmt = FormatFloatingPoint<char,TestType>;
 
+    std::cout << "*** " << Catch::getResultCapture().getCurrentTestName() << std::endl;
+
     constexpr TestType epsilon0 = 0x1p-10;
 
     constexpr TestType four = 4.0;
     constexpr TestType half = 0.5;
     constexpr TestType  two = 2.0;
-
-    std::cout << "*** " << std::setw(6) << Type<TestType>::info << " *********" << std::endl;
 
     std::cout << "epsilon0 = " << Fmt(epsilon0) << std::endl;
 
