9826a358727e505283dc239d54de6a958237c17e
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-12-30 12:14:44 +0100

    N4: Test bench for examining assembly code.

---
+++ NumericArray/unittests/src/unittest_n4_impl.cpp
@@ -0,0 +1,22 @@
+#include <N4/N4.h>
+
+struct V4fTraits {
+  static constexpr bool have_w = true;
+};
+
+using V4f = n4::Vector4f<V4fTraits>;
+
+using n4::real_t;
+
+void test_axpy(V4f& z,
+               const real_t alpha, const V4f& x, const V4f& y)
+{
+  z = alpha*x + y;
+}
+
+void test_gemv(V4f& z,
+               const real_t alpha, const n4::Matrix4f& A, const V4f& x,
+               const real_t beta, const V4f& y)
+{
+  z = alpha*(A*x) + beta*y;
+}
